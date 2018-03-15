213f025ec49b12d0551bd5457871f64c79d4ee6c
Jonas Enge <jonas.enge@gmail.com>
2018-03-15 20:41:24 +0100

    Les mer button styling

--- components/NewsItem.js
+++ components/NewsItem.js
@@ -2,7 +2,7 @@ export default ({ data }) => (
   <div className='news-item'>
     <h4>{data.title}</h4>
     <p style={{ fontSize: '14px' }} dangerouslySetInnerHTML={{__html: data.summary}} />
-    <a href={data.url} target='_blank'>Les mer</a>
+    <a className='button' href={data.url} target='_blank'>Les mer</a>
     <style jsx>
       {`
         .news-item {
@@ -12,6 +12,26 @@ export default ({ data }) => (
           background-color: #FFF;
           box-shadow: 0 2px 2px 0 rgba(0,0,0,.16), 0 0 2px 0 rgba(0,0,0,.12);
         }
+
+       .button {
+          background-color: white;
+          color: #353535;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 14px;
+          padding: 0 8px;
+          line-height: 40px
+          cursor: pointer;
+          text-transform: uppercase;
+          border-radius: 2px;
+          transition: all 0.3s ease 0s;
+        }
+
+        .button:hover {
+          background-color: #b9b9b9;
+        }
+
       `}
     </style>
   </div>
